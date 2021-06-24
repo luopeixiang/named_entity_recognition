1271b7910531d0625f72504f136784cc78cc7a09
陈敏 <chenmin@Chen-Min-MBP.local>
2021-06-24 21:20:10 +0800

    Chen Min modified.

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-
-from data import build_corpus
+from utils import format_resume
+from data import build_corpus, build_map
 from utils import extend_maps, prepocess_data_for_lstmcrf
 from evaluate import hmm_train_eval, crf_train_eval, \
     bilstm_train_and_eval, ensemble_evaluate
 
 
 def main():
+    customize = True  # 是否使用自己的数据
     """训练模型，评估结果"""
 
     # 读取数据
     print("读取数据...")
     train_word_lists, train_tag_lists, word2id, tag2id = \
         build_corpus("train")
+    print(train_tag_lists)
+    if customize:
+        train_word_lists, train_tag_lists = format_resume()
+        train_word_lists = [train_word_lists]  # 需要放在[]里
+        train_tag_lists = [train_tag_lists]
+        word2id = build_map(train_word_lists)
+        tag2id = build_map(train_tag_lists)
+    print('-------------------')
+    print(train_tag_lists)
+
     dev_word_lists, dev_tag_lists = build_corpus("dev", make_vocab=False)
     test_word_lists, test_tag_lists = build_corpus("test", make_vocab=False)
 
@@ -25,48 +36,48 @@ def main():
     )
 
     # 训练评估CRF模型
-    print("正在训练评估CRF模型...")
-    crf_pred = crf_train_eval(
-        (train_word_lists, train_tag_lists),
-        (test_word_lists, test_tag_lists)
-    )
+    # print("正在训练评估CRF模型...")
+    # crf_pred = crf_train_eval(
+    #     (train_word_lists, train_tag_lists),
+    #     (test_word_lists, test_tag_lists)
+    # )
 
     # 训练评估BI-LSTM模型
-    print("正在训练评估双向LSTM模型...")
-    # LSTM模型训练的时候需要在word2id和tag2id加入PAD和UNK
-    bilstm_word2id, bilstm_tag2id = extend_maps(word2id, tag2id, for_crf=False)
-    lstm_pred = bilstm_train_and_eval(
-        (train_word_lists, train_tag_lists),
-        (dev_word_lists, dev_tag_lists),
-        (test_word_lists, test_tag_lists),
-        bilstm_word2id, bilstm_tag2id,
-        crf=False
-    )
+    # print("正在训练评估双向LSTM模型...")
+    # # LSTM模型训练的时候需要在word2id和tag2id加入PAD和UNK
+    # bilstm_word2id, bilstm_tag2id = extend_maps(word2id, tag2id, for_crf=False)
+    # lstm_pred = bilstm_train_and_eval(
+    #     (train_word_lists, train_tag_lists),
+    #     (dev_word_lists, dev_tag_lists),
+    #     (test_word_lists, test_tag_lists),
+    #     bilstm_word2id, bilstm_tag2id,
+    #     crf=False
+    # )
 
-    print("正在训练评估Bi-LSTM+CRF模型...")
-    # 如果是加了CRF的lstm还要加入<start>和<end> (解码的时候需要用到)
-    crf_word2id, crf_tag2id = extend_maps(word2id, tag2id, for_crf=True)
-    # 还需要额外的一些数据处理
-    train_word_lists, train_tag_lists = prepocess_data_for_lstmcrf(
-        train_word_lists, train_tag_lists
-    )
-    dev_word_lists, dev_tag_lists = prepocess_data_for_lstmcrf(
-        dev_word_lists, dev_tag_lists
-    )
-    test_word_lists, test_tag_lists = prepocess_data_for_lstmcrf(
-        test_word_lists, test_tag_lists, test=True
-    )
-    lstmcrf_pred = bilstm_train_and_eval(
-        (train_word_lists, train_tag_lists),
-        (dev_word_lists, dev_tag_lists),
-        (test_word_lists, test_tag_lists),
-        crf_word2id, crf_tag2id
-    )
+    # print("正在训练评估Bi-LSTM+CRF模型...")
+    # # 如果是加了CRF的lstm还要加入<start>和<end> (解码的时候需要用到)
+    # crf_word2id, crf_tag2id = extend_maps(word2id, tag2id, for_crf=True)
+    # # 还需要额外的一些数据处理
+    # train_word_lists, train_tag_lists = prepocess_data_for_lstmcrf(
+    #     train_word_lists, train_tag_lists
+    # )
+    # dev_word_lists, dev_tag_lists = prepocess_data_for_lstmcrf(
+    #     dev_word_lists, dev_tag_lists
+    # )
+    # test_word_lists, test_tag_lists = prepocess_data_for_lstmcrf(
+    #     test_word_lists, test_tag_lists, test=True
+    # )
+    # lstmcrf_pred = bilstm_train_and_eval(
+    #     (train_word_lists, train_tag_lists),
+    #     (dev_word_lists, dev_tag_lists),
+    #     (test_word_lists, test_tag_lists),
+    #     crf_word2id, crf_tag2id
+    # )
 
-    ensemble_evaluate(
-        [hmm_pred, crf_pred, lstm_pred, lstmcrf_pred],
-        test_tag_lists
-    )
+    # ensemble_evaluate(
+    #     [hmm_pred, crf_pred, lstm_pred, lstmcrf_pred],
+    #     test_tag_lists
+    # )
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -22,6 +22,82 @@ def load_model(file_name):
     return model
 
 
+# 读取txt里的内容到list
+def read_txt_to_list():
+    res = []
+    file = open('resume.txt', 'r')
+    while 1:
+        char = file.read(1)
+        if not char:
+            break
+        res.append(char)
+    file.close()
+    return res
+
+
+# 展示结果
+def show_info(word_list, tag_list):
+    word_res = []
+    tag_res = []
+    for i in range(len(word_list)):
+        if not (tag_list[i].startswith('B') or tag_list[i].startswith('M') or tag_list[i].startswith('E')):
+            word_res.append(word_list[i])
+            tag_res.append('NULL')
+            continue
+        if tag_list[i].startswith('B'):
+            str_word = word_list[i]
+            str_tag = tag_list[i][2:]
+            tag_res.append(str_tag)
+            j = i + 1
+            while 1:
+                if tag_list[j].startswith('E'):
+                    str_word += word_list[j]
+                    break
+                else:
+                    str_word += word_list[j]
+                    j += 1
+            word_res.append(str_word)
+    print("数据组合结果: ", word_res)
+    print("数据标签: ", tag_res)
+    return word_res, tag_res
+
+
+# 修改格式
+def format_resume():
+    word_list = []
+    tag_list = []
+    with open('dr.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            word = line[0]  # 内容（一个字）
+            tag = line[2]  # 标签的开端
+            i = 3
+            while 1:
+                if line[i] == '\t':  # 从标签的开端开始，直到'\t'，说明标签结束
+                    break
+                else:
+                    tag += line[i]
+                i += 1
+            formatted_tag = ''
+            if len(tag) > 1:
+                formatted_tag = tag[0] + '-' + tag[5:].upper()
+            else:
+                formatted_tag = tag
+            word_list.append(word)
+            tag_list.append(formatted_tag)
+
+    # 把tag list标签的最后一个改成E开头，中间的改成M开头
+    if tag_list[-1].startswith('I'):
+        tag_list[-1] = 'E' + tag_list[-1][1:]
+
+    for i in range(len(tag_list) - 1):
+        if tag_list[i].startswith('I'):
+            if tag_list[i + 1].startswith('I') or tag_list[i + 1].startswith('E'):  # 后面那个以I或E开头，说明还在内部
+                tag_list[i] = 'M' + tag_list[i][1:]
+            else:  # 其他情况，说明这个标签结束了
+                tag_list[i] = 'E' + tag_list[i][1:]
+    return word_list, tag_list
+
+
 # LSTM模型训练的时候需要在word2id和tag2id加入PAD和UNK
 # 如果是加了CRF的lstm还要加入<start>和<end> (解码的时候需要用到)
 def extend_maps(word2id, tag2id, for_crf=True):
